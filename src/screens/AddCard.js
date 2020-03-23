ca2c35157d3b5f631b111d105966b47b5ae3bbfb
Ibneet <5ibneet@gmail.com>
2020-03-24 01:49:57 +0530

    firebase database setup

--- src/screens/AddCard.js
+++ src/screens/AddCard.js
@@ -1,11 +1,107 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, KeyboardAvoidingView } from 'react-native';
+import * as firebase from 'firebase'
+import { Input, Card, Button, Icon, H1 } from 'native-base'
+
+export default class AddCardScreen extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      screen: 'Today',
+      list:[],
+      activity: "",
+      backlogList: [],
+      blockedList: [],
+      doneList: [],
+      doingList: [],
+      todayList: []
+    }
+  }
+  nav=(backlog,blocked,doing,done,today,activity)=>{
+    if(backlog){
+      this.sendBacklog(activity)
+      this.setState({screen:'Backlog'},()=>{console.log(this.state.screen)})
+      
+      this.setState((state)=>{return{list: this.state.backlogList}})
+      
+    }
+    if(blocked){
+      this.setState({screen: 'Blocked'})
+      this.setState({list: blockedList})
+    }
+    if(doing){
+      this.setState({screen: 'Doing'})
+      this.setState({list: doingList})
+    }
+    if(done){
+      this.setState({screen: 'Done'})
+      this.setState({list: doneList})
+    }
+    if(today){
+      this.setState({screen: 'Today'})
+      this.setState({list: todayList})
+    }
+    return this.state.screen
+  }
+  sendBacklog = (activity) => {
+    var backlogListRef = firebase.database().ref('backlog_list')
+    var newBacklogRef = backlogListRef.push()
+    newBacklogRef.set({
+      text: activity,
+      time: Date.now()
+    })
+    this.setState({activity: ""})
+  }
+  
+  updateBacklogList = backlogList => {
+    this.setState({ backlogList: backlogList })
+  }
 
 
-export default function AddCardScreen() {
+  UNSAFE_componentWillMount(){
+    var self = this
+    var backlogListRef = firebase.database().ref('backlog_list')
+    backlogListRef.on('value', dataSnapshot => {
+      if(dataSnapshot.val()) {
+        let backlogList = Object.values(dataSnapshot.val())
+        self.updateBacklogList(backlogList)
+      }
+    })
+  }
+
+  render() {
+    const backlog = this.props.route.params?.backlog ?? false;  
+    const blocked = this.props.route.params?.blocked ?? false;
+    const doing = this.props.route.params?.doing ?? false;     
+    const done = this.props.route.params?.done ?? false;
+    const today = this.props.route.params?.today ?? false;
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Add Card Screen!</Text>
-      </View>
+      <KeyboardAvoidingView 
+      behavior='padding'
+      enabled
+      style={styles.container}>
+        <Input
+        onChangeText={text=> {
+          this.setState({activity:text})
+        }}
+        value={this.state.activity}
+        placeholder='Enter new activity'
+        />
+        <Button full rounded icon onPress={() => {
+          {this.nav(backlog,blocked,doing,done,today,this.state.activity);
+          this.props.navigation.navigate(this.state.screen,this.state.list)}
+        }}>
+          <Icon name='arrow-forward'/>
+        </Button>
+      </KeyboardAvoidingView>
     );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- src/screens/KanbanScreens/Backlog/Backlog.js
+++ src/screens/KanbanScreens/Backlog/Backlog.js
@@ -1,14 +1,46 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View, StyleSheet, KeyboardAvoidingView, FlatList } from 'react-native';
+import { Input, Card, Button, Icon } from 'native-base'
 
-
-export default function BacklogScreen({navigation}) {
+export default class DetailsScreen extends React.Component {
+  render(){ 
+    const list = this.props.route.params?.list ?? []
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Button
-        title="Go to AddCard"
-        onPress={() => navigation.navigate('AddCard')}
-      />
-      </View>
+      <KeyboardAvoidingView 
+      behavior='padding'
+      enabled
+      style={styles.container}>
+        <View style={{flex:1}}>
+        <FlatList
+        data={list}
+        inverted
+        keyExtractor={(item,index) => item.time.toString()}
+        renderItem={({item}) => (
+          <Card>
+            <Text>{item.text}</Text>
+            <Text>{item.dueDate}</Text>
+            <Text>{new Date(item.time).toLocaleDateString}</Text>
+          </Card>
+        )}
+        />
+        
+        </View>
+        <View>
+        <Button full rounded icon onPress={() => this.props.navigation.navigate('AddCard', {
+          backlog: true,
+        })}>
+          <Text>New Card?</Text>
+        </Button>
+        </View>
+      </KeyboardAvoidingView>
     );
 }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
